f772155bbf3d027fde572f84c76a26daae58227f
Bahama Nuthatch <97067842+bahamanuthatch@users.noreply.github.com>
2022-06-25 10:17:59 +0200

    .

---
+++ main/install_vm_apps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# github.com/bahamanuthatch
+# For local install, user is already created 
+# Run with sudo
+
+usr=$1
+
+# installation
+cd /home/$usr/scripts_nuthatch/newinstall/applications
+./install_brave.sh
+./install_chrome.sh
+./install_chromium.sh
+./install_protonvpncli.sh
+./install_nordvpn.sh
+./install_kloak.sh
+
+apt update
+apt dist-upgrade -y
+
+apt install -y brave-browser
+apt install protonvpn-cli
+apt install nordvpn
+sudo apt install --no-install-recommends kloak
+apt install -y kate
+apt install -y geany
+
+./install_veracrypt.sh
+
