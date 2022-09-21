bd5c67aa34405ef926654cb9cafb073d3c80990c
bahamanuthatch <bahamanuthatch@protonmail.com>
2022-09-21 13:29:51 +0200

    .

--- applications/install_rocketpool.sh
+++ applications/install_rocketpool.sh
@@ -3,9 +3,10 @@
 # Run with sudo
 
 usr=$1
+app=/home/$usr/bin/rocketpool
 
 mkdir -p /home/$usr/bin
-wget https://github.com/rocket-pool/smartnode-install/releases/latest/download/rocketpool-cli-linux-amd64 -O /home/$usr/bin/rocketpool
-chmod +x /home/$usr/bin/rocketpool
-su -c '/home/$usr/bin/rocketpool service install' $usr
+wget https://github.com/rocket-pool/smartnode-install/releases/latest/download/rocketpool-cli-linux-amd64 -O $app
+chmod +x $app
+su -c "$app service install" $usr
 
