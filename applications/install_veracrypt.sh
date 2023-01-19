6811c647444388165ba533f1e056a928a9e501f1
Bahama Nuthatch <97067842+bahamanuthatch@users.noreply.github.com>
2023-01-19 17:09:59 +0100

    .

--- applications/install_veracrypt.sh
+++ applications/install_veracrypt.sh
@@ -3,13 +3,13 @@
 # Run with sudo
 # Run last, and print out verification
 
-wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb
+wget https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-22.04-amd64.deb
 
-wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-Ubuntu-20.04-amd64.deb.sig
+wget https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-Ubuntu-22.04-amd64.deb.sig
 
-wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-sha512sum.txt
+wget https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-sha512sum.txt
 
-wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-1.24-Update7-sha512sum.txt.sig
+wget https://launchpad.net/veracrypt/trunk/1.25.9/+download/veracrypt-1.25.9-sha512sum.txt.sig
 
 wget https://www.idrix.fr/VeraCrypt/VeraCrypt_PGP_public_key.asc
 
