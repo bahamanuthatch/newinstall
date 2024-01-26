d5ad2f72c762891a9584d8a9d60acc7cd72b7840
Bahama Nuthatch <97067842+bahamanuthatch@users.noreply.github.com>
2024-01-26 12:43:31 +0000

    .

--- main/install_vm02_apps.sh
+++ main/install_vm02_apps.sh
@@ -8,21 +8,14 @@ usr=$1
 # installation
 cd /home/$usr/scripts_nuthatch/newinstall/applications
 ./install_brave.sh
-./install_chrome.sh
-./install_chromium.sh
-./install_protonvpncli.sh
-#./install_nordvpn.sh $usr
 
 apt update
 apt dist-upgrade -y
 
 apt install -y brave-browser
-apt install -y protonvpn-cli
 apt install -y geany
 apt install -y featherpad
 
-#apt remove --purge -y libreoffice*
-#apt install -y gnumeric
 
 # last
 ./install_veracrypt.sh
