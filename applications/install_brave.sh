854967bd156a32780d45d783ebd544f43339bb3f
Bahama Nuthatch <97067842+bahamanuthatch@users.noreply.github.com>
2024-01-26 12:45:14 +0000

    .

--- applications/install_brave.sh
+++ applications/install_brave.sh
@@ -2,9 +2,9 @@
 # github.com/bahamanuthatch
 # Run with sudo
 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 #apt update
 
